81612a2ffffb1519819ff1252c93bbfef855631d
Zach Aubin <aubin@rams.colostate.edu>
2020-02-15 12:19:36 -0700

    script nodes

--- start-nodes.sh
+++ start-nodes.sh
@@ -1,13 +1,13 @@
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-JAR_PATH="$DIR/conf/:$DIR/build/libs/<peer-to-peer-network>.jar"
+DIR="$( cd "$( git/cs455.overlay "$0" )" && pwd )"
+JAR_PATH="$DIR/conf/:$DIR/build/libs/cs455.overlay-1.0-SNAPSHOT.jar"
 MACHINE_LIST="$DIR/conf/machine_list"
 
-SCRIPT="java -cp $JAR_PATH cs455.overlay.node.MessagingNode <registry-host> <registry-port>"
+SCRIPT="java -cp $JAR_PATH cs455.overlay.node.MessagingNode $1 $2"
 COMMAND='gnome-terminal --geometry=200x40'
 
-for machine in`cat $MACHINE_LIST`
+for machine in $(cat $MACHINE_LIST)
 do   
-    OPTION='--tab -t "'$machine'" -e "ssh -t '$machine' cd '$DIR'; echo '$SCRIPT'; '$SCRIPT'"'   
+    OPTION='--tab -t "'$machine'" -- "ssh -t '$machine' cd '$DIR'; echo '$SCRIPT'; '$SCRIPT'"'   
     COMMAND+=" $OPTION"
 done
 eval $COMMAND &
